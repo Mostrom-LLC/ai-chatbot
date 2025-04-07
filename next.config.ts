d7438affb80acffe6c9490a2f48e32270ee13218
Kaise White <kaisewhite@gmail.com>
2025-04-07 10:01:35 -0400

    Add tsx extension and remove redundant not-found.tsx page

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,9 @@ const nextConfig: NextConfig = {
         // Instead, use `not-found.ts` as a workaround
         // "ts" is required to resolve `not-found.ts`
         // https://github.com/vercel/next.js/issues/65447
-        "ts"
+        // Standard Next.js page extensions
+    'tsx',
+    'ts',
     ],
   images: {
     remotePatterns: [
