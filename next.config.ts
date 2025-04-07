ee2f3737b9edb6518ac2906510f1881817c219d1
Kaise White <kaisewhite@gmail.com>
2025-04-07 14:12:04 -0400

    Add dynamic page extensions based on environment and dotenv config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,22 @@
 import type { NextConfig } from 'next';
 
+import { config }  from 'dotenv';
+
+config({ path: '.env.local' });
+
+  // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+        // Instead, use `not-found.ts` as a workaround
+        // "ts" is required to resolve `not-found.ts`
+// https://github.com/vercel/next.js/issues/65447
+        
+let pageExtensions = process.env.NEXT_PUBLIC_APP_NODE_ENV === 'production' ? ['js', 'jsx', 'ts', 'tsx', 'page.tsx', 'page.ts']:['page.tsx', 'page.ts', 'ts'];
+console.log(`[DEBUG] pageExtensions: ${pageExtensions}`)
+console.log(`[DEBUG] NEXT_PUBLIC_APP_NODE_ENV: ${process.env.NEXT_PUBLIC_APP_NODE_ENV}`)
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
-  pageExtensions: [
-        "page.tsx",
-        "page.ts",
-        // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
-        // Instead, use `not-found.ts` as a workaround
-        // "ts" is required to resolve `not-found.ts`
-        // https://github.com/vercel/next.js/issues/65447
-        "ts"
-    ],
+  pageExtensions: pageExtensions,
   images: {
     remotePatterns: [
       {
